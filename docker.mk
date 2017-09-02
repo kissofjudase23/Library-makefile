78885326c904c35113bb7630d986b0ffb2e0afe7
e23 <kissofjudase23@gmail.com>
2017-09-02 17:38:37 +0800

    Use variables of repo and tag

--- docker.mk
+++ docker.mk
@@ -4,19 +4,20 @@ SHELL_COMMAND :=
 # build a docker image from the docker file
 docker_build:
 	docker build \
-	-t $(TAG) -f $(DOCKERFILE) .
+		-t $(DOCKER_REPO):$(DOCKER_TAG) \
+		-f $(DOCKERFILE) .
 
 # create a container and allocate a pseudo tty for debug
 docker_debug_run:
 	docker run --rm \
-	-v $(HOST_PATH):$(GUEST_PATH) \
-	-it $(TAG) $(SHELL)
+		-v $(HOST_PATH):$(GUEST_PATH) \
+		-i --tty $(DOCKER_REPO):$(DOCKER_TAG) $(SHELL)
 
 # create a container and run your test command via bash
 docker_internal_run:
 	docker run --rm \
 	-v $(HOST_PATH):$(GUEST_PATH) \
-	-i $(TAG) $(SHELL) $(SHELL_COMMAND)
+	-i $(DOCKER_REPO):$(DOCKER_TAG) $(SHELL) $(SHELL_COMMAND)
 
 # remove dangling docker images
 docker_dangling_rmi:
